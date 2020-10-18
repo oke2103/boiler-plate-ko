5ccacd82c46d8e496a82342d5870e8307820efc8
Howan <oke2103@gmail.com>
2020-10-18 22:36:55 +0900

    서버 정보 보호

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const app = express();
-
-// 방화벽 관련 module
-// const cors = require('cors');
-
 const bodyParser = require('body-parser');
-const port = process.env.PORT || 5000;
+const port = '5000';
 
+const config = require('./config/key');
+
+const {User} = require('./model/User');
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://193.122.126.92:27017/test');
+
+console.log(config);
+
+mongoose.connect(config.mongoURL);
 var db = mongoose.connection
 .on('error', function(){
     console.log('Connection Failed!');
@@ -18,8 +20,20 @@ var db = mongoose.connection
 });
 
 // app.use(cors());
-
+app.use(bodyParser.urlencoded({extended : true}));
 app.use(bodyParser.json());
+
+app.post('/register', (req,res) => {
+    console.log("atest");
+    // 회원가입 정보
+    const user = new User(req.body);
+    user.save((err,doc) => {
+        if(err) return res.json({success : false,err});
+        return res.status(200).json({
+            success : true
+        })
+    })
+})
 
 app.listen(port, () => {
 	console.log(`express is running on ${port}`);
